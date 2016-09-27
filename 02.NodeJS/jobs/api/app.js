b6acd0471771c0f8648394bb3c080731381d3dc7
Marcel Nakamine <marcel.nakamine@outlook.com>
2016-09-27 20:42:40 -0300

    creating a server

--- 02.NodeJS/jobs/api/app.js
+++ 02.NodeJS/jobs/api/app.js
@@ -1,3 +1,11 @@
 'use strict';
 
 require('./init-database');
+
+const http = require('http');
+const port = 4000;
+
+const server = http.createServer();
+server.listen(port, () => {
+  console.log(`listen port ${port}`);
+});
