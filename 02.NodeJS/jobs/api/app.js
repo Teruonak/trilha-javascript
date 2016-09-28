30580fa6b57c64b09409eef7ae918d3b07ed4a62
Marcel Nakamine <marcel.nakamine@outlook.com>
2016-09-27 21:43:52 -0300

    adding a controller for company

--- 02.NodeJS/jobs/api/app.js
+++ 02.NodeJS/jobs/api/app.js
@@ -4,23 +4,17 @@ require('./init-database');
 
 const http = require('http');
 const port = 4000;
-const Company = require('./model/company-model');
 const router = require('./router');
 
+// Config Server
 const server = http.createServer((req, res) => {
   router.proccessRequest(req,res);
 });
 
-router.get('/companies', (req,res) => {
-  res.write(JSON.stringify(Company.get()));
-  res.end();
-});
+// Config Controllers
+require('./controllers/company-controller');
 
-router.get('/jobs', (req,res) => {
-  res.write('<h1>Jobs</h1>');
-  res.end();
-})
-
+// Bind Server
 server.listen(port, () => {
   console.log(`listen port ${port}`);
 });
