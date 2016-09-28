7b1bd5195ec26d5c85aad76475bbc21e0021376c
Marcel Nakamine <marcel.nakamine@outlook.com>
2016-09-27 21:14:22 -0300

    retrieve companies
    the url is: /companies

--- 02.NodeJS/jobs/api/app.js
+++ 02.NodeJS/jobs/api/app.js
@@ -4,9 +4,12 @@ require('./init-database');
 
 const http = require('http');
 const port = 4000;
+const Company = require('./model/company-model');
 
 const server = http.createServer((req, res) => {
-
+  if (req.url === '/companies') {
+    res.write(JSON.stringify(Company.get()));
+  }
   res.end();
 });
 server.listen(port, () => {
