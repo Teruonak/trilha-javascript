67aef83f0ff2d9d3e9700665e6c0e594c3d28ca9
Marcel Nakamine <marcel.nakamine@outlook.com>
2016-09-27 21:02:16 -0300

    adding a request and response

--- 02.NodeJS/jobs/api/app.js
+++ 02.NodeJS/jobs/api/app.js
@@ -5,7 +5,10 @@ require('./init-database');
 const http = require('http');
 const port = 4000;
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+
+  res.end();
+});
 server.listen(port, () => {
   console.log(`listen port ${port}`);
 });
