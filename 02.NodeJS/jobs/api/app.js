2e88e4b7d3b14abc02ebd0f6d022ccffeffab90c
Marcel Nakamine <marcel.nakamine@outlook.com>
2016-09-29 20:10:23 -0300

    modularized the start server method

--- 02.NodeJS/jobs/api/app.js
+++ 02.NodeJS/jobs/api/app.js
@@ -4,26 +4,8 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
-app.listen(port, () => {
-  console.log(`listen on port ${port}`);
-})
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
 
-//
-// require('./init-database');
-//
-// const http = require('http');
-// const router = require('./router');
-//
-// // Config Server
-// const server = http.createServer((req, res) => {
-//   router.proccessRequest(req,res);
-// });
-//
-// // Config Controllers
-// require('./controllers/company-controller');
-// require('./controllers/user-controller');
-//
-// // Bind Server
-// server.listen(port, () => {
-//   console.log(`listen port ${port}`);
-// });
+module.exports = app;
