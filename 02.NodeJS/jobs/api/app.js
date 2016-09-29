4c30849ba355ae7f987b4e79c4a314416496c1eb
Marcel Nakamine <marcel.nakamine@outlook.com>
2016-09-29 20:38:48 -0300

    update the controller/router for company

--- 02.NodeJS/jobs/api/app.js
+++ 02.NodeJS/jobs/api/app.js
@@ -3,8 +3,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.use(require('./controllers/company-controller'));
 
 module.exports = app;
--- 02.NodeJS/jobs/api/controllers/company-controller.js
+++ 02.NodeJS/jobs/api/controllers/company-controller.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const express = require('express');
+const router = express.Router();
 const Company = require('../model/company-model');
-const router = require('../router');
 
 router.get('/companies', (req,res) => {
-  res.write(JSON.stringify(Company.get()));
-  res.end();
+  res.Company.get();
 });
+
+module.exports = router;
