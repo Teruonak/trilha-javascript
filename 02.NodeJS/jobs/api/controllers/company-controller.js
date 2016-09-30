0ef07c36241d87146ea6e242f738119ee4682a4c
Marcel Nakamine <marcel.nakamine@outlook.com>
2016-09-29 21:05:09 -0300

    removing unused imports

--- 02.NodeJS/jobs/api/controllers/company-controller.js
+++ 02.NodeJS/jobs/api/controllers/company-controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const express = require('express');
 const Company = require('../model/company-model');
 const ctrl = {};
+
 ctrl.getCompanies = (req,res) => {
   res.send(Company.get());
 };
