5f4931ee5ce28fb35f06d1db354d3a1aa82c0cf7
Marcel Nakamine <marcel.nakamine@outlook.com>
2016-10-22 14:22:10 -0200

    deleted files to sync with upstream

--- 02.NodeJS/jobs/api/routes/auth-route.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const express = require('express');
-const router = express.Router();
-const ctrl = require('../controllers/auth-controller');
-
-router.post('/auth', ctrl.authUser);
-
-module.exports = router;
